1ee5aa72dd96f67a3efc5dab3a0ba5f7f932e40f
AleAmoros <amorosale98@gmail.com>
2023-04-25 17:27:30 +0200

    Adding to libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: aamoros- <aamoros-@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 15:30:18 by aamoros-          #+#    #+#             */
-/*   Updated: 2023/04/25 16:29:54 by aamoros-         ###   ########.fr       */
+/*   Updated: 2023/04/25 17:27:18 by aamoros-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,24 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int	memcmp(const void *s1, const void *s2, size_t n);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
 int	ft_isprint(int c);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+char *strnstr(const char *haystack, const char *needle, size_t len);
 
 #endif
